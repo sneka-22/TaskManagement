a2fc431df504b4306579c7d09c583c8bf8f516b0
SnekaGurusamy <snekag@centizen.com>
2025-06-19 18:20:56 +0530

    swgger json changes

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 var express = require('express');
+import type { Request, Response } from 'express';
 const serverConfig = require('./config');
 require('./dbconfig');
 const passport = require("passport"); 
@@ -12,6 +13,12 @@ app.use(express.json());
 app.use(passport.initialize());
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpecs = require('./config/swagger');
+
+// Export OpenAPI spec as JSON
+app.get('/api-docs.json', (req:Request, res:Response) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpecs);
+});
 
 // Swagger documentation route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, {
